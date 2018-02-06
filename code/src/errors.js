eff7be18f1be5f513f0f8d9712f2f41c2dc96b97
Shlomi Sasson <shlomi.s@walkme.com>
2018-02-06 11:55:23 +0200

    fix

--- code/src/errors.js
+++ code/src/errors.js
@@ -2,15 +2,13 @@
  * Created by Shlomi on 05/09/2015.
  */
 
-import helper from './utils/helper';
-
 (()=> {
     try{
         setTimeout(()=>{
             JSON.parse('bla bla bla');
         })
     }catch(e){
-        helper.error(e);
+        console.error(e);
     }
 
     // What's the problem and how to fix it?
