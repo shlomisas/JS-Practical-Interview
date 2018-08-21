3732dcd0cb256de9a8d325cfdbd65c9e4c6005ad
Shlomi Sasson <shlomi.s@walkme.com>
2018-08-21 13:39:33 +0300

    fix

--- code/src/errors.js
+++ code/src/errors.js
@@ -2,6 +2,25 @@
  * Created by Shlomi on 05/09/2015.
  */
 
+(async() => {
+
+    // What's the problem with this code?
+
+    try {
+
+        new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+                reject(new Error('something went wrong..'));
+            }, 0);
+
+        });
+
+    } catch (e) {
+        console.error(e);
+    }
+})();
+
 (async ()=> {
 
     function foo(){}
