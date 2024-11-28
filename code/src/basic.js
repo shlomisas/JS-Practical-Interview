98224d4cd5bda4f2c6f07fa3f0463a953578de61
Shlomi Sasson <shlomi@sasson.me>
2024-11-28 11:31:41 +0200

    fix

--- code/src/basic.js
+++ code/src/basic.js
@@ -11,13 +11,13 @@
 (() => {
 
     /**
-     * The task: loop thru `arr` to access the indices and values
+     * The task: loop thru `arr` and print indices and values
      */
 
     const arr = [1, 2, 3];
 
     /**
-     * The task: loop thru `obj` to access the key and values
+     * The task: loop thru `obj` and print key and values
      */
 
     const obj = {a: 1, b: 2, c: 3};
